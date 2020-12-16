b6249fd6b0fc02eaede7502fc9a0de8b4836657e
tatsOre <tatianaorezap@gmail.com>
2020-12-15 19:05:31 -0500

    Insert node in sorted Linked List

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -4,7 +4,6 @@
  * insert_node - Inserts a number into a sorted singly linked list
  * @head: pointer to head of list
  * @number: number to insert in the linked list
- * head:
  * Return: the address of the new node, or NULL if it failed
  */
 listint_t *insert_node(listint_t **head, int number)
