36d84b0dcf6c13927d6a66cadc8b6b45ebe8479b
tatsOre <tatianaorezap@gmail.com>
2020-12-15 19:03:41 -0500

    Insert node in sorted Linked List

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * insert_node - Inserts a number into a sorted singly linked list
+ * @head: pointer to head of list
+ * @number: number to insert in the linked list
+ * head:
+ * Return: the address of the new node, or NULL if it failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *current = *head, *new_node = NULL;
+
+	if (!head)
+		return (NULL);
+
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = number;
+	new_node->next = NULL;
+
+	if (!*head || current->n >= number)
+	{
+		new_node->next = *head;
+		return (*head = new_node);
+	}
+	while (current)
+	{
+		if (!current->next || current->next->n >= number)
+		{
+			new_node->next = current->next;
+			return (current->next = new_node);
+		}
+		current = current->next;
+	}
+	return (NULL);
+}
---
+++ 0x01-insert_in_sorted_linked_list/README.md
@@ -0,0 +1,9 @@
+# 0x01. Insert in sorted linked list
+
+Write a function in C that inserts a number into a sorted singly linked list.
+
+### Author:
+* Tatiana Orejuela Zapata | [Github](https://github.com/tatsOre)
+
+##### Specializations - Interview Preparation ― Algorithms
+##### December, 2020. Cali, Colombia.
