aaf4be246e2ef26671474df8aa184f07c513a2cc
tatsOre <tatianaorezap@gmail.com>
2020-12-22 15:51:28 -0500

    Insert new node in a binary tree

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - inserts a node
+ * @parent: pointer to the node to insert the right-child in
+ * @value: value to store in the new node
+ * Return: pointer to the created node, or NULL on failure or if parent is NULL
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+
+	if (!new)
+		return (NULL);
+
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+	return (new);
+}
---
+++ 0x02-heap_insert/README.md
@@ -0,0 +1,7 @@
+# 0x02. Heap Insert
+
+### Author:
+* Tatiana Orejuela Zapata | [Github](https://github.com/tatsOre)
+
+##### Specializations - Interview Preparation ― Algorithms
+##### December, 2020. Cali, Colombia.
