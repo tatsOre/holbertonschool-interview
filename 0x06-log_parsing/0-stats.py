0e9d0de9f9efc9c3998123c663c5e856514aa5e9
tatsOre <tatianaorezap@gmail.com>
2021-01-27 09:16:40 -0500

    Log parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
 """ Module for Log Parsing Script """
-import re
 import sys
 
 
 if __name__ == '__main__':
-    n_lines = 0
+    log = {'200': 0, '301': 0, '400': 0, '401': 0,
+           '403': 0, '404': 0, '405': 0, '500': 0}
     file_size = 0
-    log = {}
 
     def print_log():
         """ Method that prints metrics """
         print("File size:", file_size)
         for code in sorted(log.keys()):
-            print(f"{code}: {log[code]}")
+            if log[code]:
+                print(f"{code}: {log[code]}")
     try:
+        n_lines = 0
         for line in sys.stdin:
+            try:
+                response = line.split()
+                code = response[-2]
+                size = response[-1]
+                if code in log:
+                    log[code] += 1
+                    file_size += int(size)
+            except:
+                pass
+
             n_lines += 1
-            RESPONSE = re.findall(r'\s\d{3}', line)
-            CODE = RESPONSE[0].strip()
-            SIZE = 0 if len(RESPONSE) < 2 else RESPONSE[1].strip()
-            if CODE:
-                log[CODE] = log.get(CODE, 0) + 1
-                file_size += int(SIZE)
 
             if n_lines == 10:
                 n_lines = 0
