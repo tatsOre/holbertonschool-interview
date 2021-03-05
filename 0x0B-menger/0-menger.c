6c8d0f6ec65e311363e33a59a0a8d3ca0c307901
tatsOre <tatianaorezap@gmail.com>
2021-03-04 19:20:29 -0500

    2D Menger sponge

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,45 @@
+#include "menger.h"
+
+/**
+ * fill - Verifies if one 'cell' should be filled or not.
+ *
+ * @row:  position x
+ * @col:  position y
+ * 
+ * Return: 1 to fill | 0 to keep empty
+ */
+int fill(int row, int col)
+{
+	while (row > 0 || col > 0)
+	{
+		if (row % 3 == 1 && col % 3 == 1)
+			return (0);
+		row /= 3;
+		col /= 3;
+	}
+	return (1);
+}
+/**
+ * menger - Draws a 2D Menger Sponge, Sierpiński carpet.
+ *
+ * @level:  level of the Menger Sponge to draw
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+void menger(int level)
+{
+	int n, row, col;
+
+	n = pow(3, level);
+	for (row = 0; row < n; row++)
+	{
+		for (col = 0; col < n; col++)
+		{
+			if (fill(row, col))
+				putchar('#');
+			else
+				putchar(' ');
+		}
+		putchar('\n');
+	}
+}
---
+++ 0x0B-menger/menger.h
@@ -0,0 +1,11 @@
+#ifndef MENGER_H
+#define MENGER_H
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int fill(int row, int col);
+void menger(int level);
+
+#endif  /* MENGER_H */
