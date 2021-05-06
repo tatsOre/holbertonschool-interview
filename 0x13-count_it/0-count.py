d7cdc3d56efd4d40ae56f08836d9e4090bbadd50
tatsOre <tatianaorezap@gmail.com>
2021-05-06 18:19:14 -0500

    Count it

---
+++ 0x13-count_it/0-count.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Recursive function that queries the Reddit API and prints the count
+of the frequency of specific words in the titles of all hot articles
+for a given subreddit"""
+import requests
+
+
+def count_words(subreddit, word_list, allposts={}, after=""):
+    """
+    Prints a sorted count of given keywords
+    (<word_list>: case-insensitive, delimited by spaces)
+    """
+    base_url = 'https://www.reddit.com/r/'
+    endpoint = f'{base_url}{subreddit}.json?after={after}'
+    headers = {'user-agent': 'holbie1626_t100_2021'}
+    word_list = set([word.lower() for word in word_list])
+
+    response = requests.get(endpoint, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return None
+
+    data = response.json()
+    after = data.get('data').get('after')
+    posts = data.get('data').get('children')
+    if posts:
+        for post in posts:
+            title = post.get('data').get('title').lower().split()
+            for word in title:
+                if word in word_list:
+                    allposts[word] = allposts.get(word, 0) + 1
+    if after:
+        return count_words(subreddit, word_list, allposts, after)
+    else:
+        # sorted returns a list of tuples
+        # [('java', 27), ('javascript', 27), ('python', 53), ...]
+        sorted_dict = dict(sorted(allposts.items()))
+        # cast the list to dictionary
+        for key, value in sorted_dict.items():
+            print(f"{key}: {value}")
+        return sorted_dict
---
+++ 0x13-count_it/README.md
@@ -0,0 +1,13 @@
+# 0x13. Count it!
+
+Write a recursive function that queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords (case-insensitive, delimited by spaces. Javascript should count as javascript, but java should not).
+
+* Results should be printed in descending order, by the count, and if the count is the same for separate keywords, they should then be sorted alphabetically (ascending, from A to Z). Words with no matches should be skipped and not printed. Words must be printed in lowercase.
+* Results are based on the number of times a keyword appears, not titles it appears in. `java java java` counts as 3 separate occurrences of `java`.
+* NOTE: Invalid subreddits may return a redirect to search results. Ensure that you are NOT following redirects.
+
+### Author:
+* Tatiana Orejuela Zapata | [Github](https://github.com/tatsOre)
+
+##### Specializations - Interview Preparation ― Algorithms
+##### May, 2021.
